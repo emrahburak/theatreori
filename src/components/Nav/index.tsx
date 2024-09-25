e56467ebe70e6000df6227564043791f40478f89
emrahburak <gemrahburak@gmail.com>
2024-09-25 18:51:18 +0300

    ADDED  Link component for logo

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -16,7 +16,9 @@ const navData = {
 export default function Nav() {
 	return <>
 		<div className="max-w-screen-xl mx-auto py-2 flex  flex-nowrap h-full items-center justify-start">
-			<Image src={logo} alt={navData.url} width={80} height={80} className="rounded-sm row-span-3" />
+			<Link href="/">
+				<Image src={logo} alt={navData.url} width={80} height={80} className="rounded-sm row-span-3" />
+			</Link>
 			<div className="pl-10">
 				<p className="font-anton text-3xl text-white  font-bold">{navData.name}</p>
 			</div>
